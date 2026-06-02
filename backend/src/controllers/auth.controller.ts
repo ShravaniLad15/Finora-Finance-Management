bc89b4366e825995e3ebca887dbaa6e913de5888
ShravaniLad15 <shravanilad.ds@gmail.com>
2026-06-02 10:11:09 +0530

    login service added

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -1,16 +1,34 @@
 import { HTTPSTATUS } from "../config/http.config";
 import { asyncHandler } from "../middleware/asyncHandler.middleware";
 import { Request, Response } from "express";
-import { registerSchema } from "../validators/auth.validator";
-import { registerService } from "../services/auth.service";
+import { loginSchema, registerSchema } from "../validators/auth.validator";
+import { loginService, registerService } from "../services/auth.service";
 
 
 export const registerController = asyncHandler(async(req: Request, res: Response) => {
   const body = registerSchema.parse(req.body)
-  await registerService(body);
+  const result = await registerService(body);
   return res
     .status(HTTPSTATUS.CREATED)
-    .json({ message: "User registered Successfully"});
+    .json({ 
+      message: "User registered Successfully",
+      data: result,});
 
 }
 );
+
+export const loginController = asyncHandler(
+  async (req: Request, res: Response) => {
+    const body = loginSchema.parse({
+      ...req.body,
+    });
+    const { user, accessToken, expiresAt, reportSetting } = await loginService(body);
+    return res.status(HTTPSTATUS.OK).json({
+      message: "User logged in successfully",
+      user,
+      accessToken,
+      expiresAt,
+      reportSetting,
+    });
+  }
+)
