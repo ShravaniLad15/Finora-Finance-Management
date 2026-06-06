2fb7a5c8e06fd641c18a716ab16f550d2fe5fde9
ShravaniLad15 <shravanilad.ds@gmail.com>
2026-06-07 01:35:46 +0530

    Multer+cloudinary

--- backend/src/middleware/errorHandler.middleware.ts
+++ backend/src/middleware/errorHandler.middleware.ts
@@ -3,6 +3,7 @@ import { HTTPSTATUS } from "../config/http.config";
 import { AppError } from "../utils/app-error";
 import {z, ZodError } from "zod";
 import { ErrorCodeEnum } from "../enums/error-code.enum";
+import { MulterError } from "multer";
 
 const formatZodError = (res: Response, error: z.ZodError) => {
   const errors = error?.issues?.map((err) => ({
@@ -16,11 +17,35 @@ const formatZodError = (res: Response, error: z.ZodError) => {
   })
 }
 
+const handleMulterError = (error: MulterError)=> {
+  const messages = {
+    LIMIT_UNEXPECTED_FILE: "Invalid fil field name. Please use 'file'",
+    LIMIT_FILE_SIZE: "File size exceeds the limit",
+    LIMIT_FILE_COUNT: "Too many files uploaded",
+    default: "File upload error",
+  };
+
+  return {
+    status: HTTPSTATUS.BAD_REQUEST,
+    message: messages[error.code as keyof typeof messages] || messages.default,
+    error: error.message,
+  };
+};
+
 export const errorHandler : ErrorRequestHandler = (error, req, res, next): any =>  {
   console.log("Error occured on PATH:", req.path,"Error:", error);
 
   if(error instanceof ZodError){
     return formatZodError(res, error);
+  }
+
+  if (error instanceof MulterError) {
+    const {status, message, error: err} = handleMulterError(error)
+    return res.status(status).json({
+      message,
+      error: err,
+      errorCode: ErrorCodeEnum.FILE_UPLOAD_ERROR,
+    });
   }
 
   if(error instanceof AppError){
